75d2c255e3c9c72d9cde5cf7a364b01b7142fa47
j aguilan <johnrenzelaguilan28@gmail.com>
2023-01-12 11:57:55 +0800

    react expense tracker

---
+++ screens/AllExpenses.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { StyleSheet, Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+
+function AllExpenses() {
+  const expensesCtx = useContext(ExpensesContext)
+  return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod='Total' />
+}
+
+export default AllExpenses;
+
+const styles = StyleSheet.create({})
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses-context';
+import { getDateMinusDays } from '../util/date';
+
+function RecentExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7daysAgo = getDateMinusDays(today, 7);
+
+    return expense.date >= date7daysAgo && expense.date <= today;
+  });
+
+  return (
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 Days' />
+  );
+}
+
+export default RecentExpenses;
+
+const styles = StyleSheet.create({});
